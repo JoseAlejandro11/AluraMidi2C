6aac394b73a127093d0bf064681d82b0ff394b2b
JoseAlejandro11 <128390019+JoseAlejandro11@users.noreply.github.com>
2023-10-20 11:05:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+Function tocaSom(SeletorAudio) {
+  const elemento= document.query Seletor(seletorAudio);
+  if (elements&& elemento.localName ==='Audio'){
+   elemento.play();
+  }
+  else{
+    console.log('Elemento não encontrado ou seletor invalido');
+  }
+}
+const listaDeteclas = document.querySeletorAll('.tecla');
+for(let contador=O;contador<listaDeteclas.length;contador++){
+  const tecla=listaDeteclas[contador];
